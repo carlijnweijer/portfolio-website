53a967c7afd67346233cfe2ac20cf5a5234655c7
carlijnweijer <carlijnweijer@gmail.com>
2021-01-18 22:45:13 +0100

    Add text about me

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { animated, interpolate, useSpring } from "react-spring";
+import metBenz from "../assets/metBenz.jpg";
 import { Navigation } from "../components/Navigation";
 import "../styles/about.scss";
 
@@ -13,13 +14,13 @@ export const About: React.FC<AboutProps> = ({ id }) => {
     y - window.innerHeight / 2,
   ];
   const trans = (x: number, y: number) =>
-    `translate3d(${x / 10}px,${y / 10}px,0)`;
-
+    `translate3d(${x / -10}px,${y / 20}px,0)`;
   const trans2 = (x: number, y: number) =>
-    `translate3d(${x / -10}px,${y / 8}px,0)`;
-
+    `translate3d(${x / -20}px,${y / 30}px,0)`;
   const trans3 = (x: number, y: number) =>
-    `translate3d(${x / 12}px,${y / -6}px,0)`;
+    `translate3d(${x / -4}px,${y / -10}px,0)`;
+  const trans4 = (x: number, y: number) =>
+    `translate3d(${x / -16}px,${y / -10}px,0)`;
 
   const [props, set] = useSpring(() => ({
     xy: [0, 0],
@@ -46,10 +47,13 @@ export const About: React.FC<AboutProps> = ({ id }) => {
           // @ts-ignore
           style={{ transform: interpolate(props.xy, trans) }}
         >
-          <h5>I'm a Junior Web Developer driven by passion and curiosity.</h5>
+          <h5>A little bit of background info</h5>
           <p>
-            I believe in lifelong learning, constructive feedback and an open
-            mind.
+            I'm 26 years old, I live in The Hague and I'm the proud mom of a 6
+            years old Golden Retriever named{" "}
+            <span className="tooltip">
+              Benz <img src={metBenz} className="tooltippic" alt="benz" />
+            </span>
           </p>
         </animated.div>
         <animated.div
@@ -57,11 +61,12 @@ export const About: React.FC<AboutProps> = ({ id }) => {
           // @ts-ignore
           style={{ transform: interpolate(props.xy, trans2) }}
         >
-          <h5>I can't wait to buy a bigger backpack!!</h5>
+          <h5 style={{ fontSize: "1.5rem" }}>
+            I'm driven by passion and curiosity
+          </h5>
           <p>
-            At Codaisseur I filled my backpack with knowledge, tricks and tools.
-            I now have the ambitions to buy a bigger backpack soon! I'm
-            currently learning TypeScript and React Native.
+            I believe in lifelong learning, constructive feedback and an open
+            mind.
           </p>
         </animated.div>
         <animated.div
@@ -76,6 +81,17 @@ export const About: React.FC<AboutProps> = ({ id }) => {
             come together. Programming is like solving a complex puzzle and
             painting a masterpiece at the same time!
           </p>
+        </animated.div>
+        <animated.div
+          className="about-card"
+          // @ts-ignore
+          style={{ transform: interpolate(props.xy, trans4) }}
+        >
+          <h5>
+            Especially interested in Front-end development and working on cool
+            projects with cool people
+          </h5>
+          <p></p>
         </animated.div>
       </div>
     </div>
